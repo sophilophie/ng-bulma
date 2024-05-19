3fa18db683446cd95994549194e0015e070ac487
sophilophie <sophia@sophiamckell.com>
2024-05-18 22:21:51 -0600

    fix prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
-	selector: 'ng-b-root',
+	selector: 'ngb-root',
 	standalone: true,
 	imports: [RouterOutlet],
 	templateUrl: './app.component.html',
